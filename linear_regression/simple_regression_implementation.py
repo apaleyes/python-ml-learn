241304a710bc1f9c47cbcf282a15e3ffdcf92b9d
Andrei Paleyes <apalees@gmail.com>
2016-11-08 22:56:07 +0000

    Added calculation of b and plotting result

--- linear_regression/simple_regression_implementation.py
+++ linear_regression/simple_regression_implementation.py
@@ -1,13 +1,19 @@
 from statistics import mean
 import numpy as np
+import matplotlib.pyplot as plt
 
-def best_fit_slope(xs, ys):
+def best_fit_line(xs, ys):
 	m = ( (mean(xs) * mean(ys) - mean(xs * ys)) /
 		  ((mean(xs)**2) - mean(xs**2)) )
-	return m
+	b = mean(ys) - m * mean(xs)
+	return m, b
 
 xs = np.array([1,2,3,4,5])
 ys = np.array([5,4,6,5,6])
 
-m = best_fit_slope(xs, ys)
-print(m)
+m, b = best_fit_line(xs, ys)
+
+regression_line = [m*x+b for x in xs]
+plt.scatter(xs, ys)
+plt.plot(xs, regression_line)
+plt.show()
