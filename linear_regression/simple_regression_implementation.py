3141e7a2fe4fd21d512d51bb1f3d08cb7488595f
Andrei Paleyes <apalees@gmail.com>
2016-11-09 01:26:35 +0000

    Coeff of determination, test date set

--- linear_regression/simple_regression_implementation.py
+++ linear_regression/simple_regression_implementation.py
@@ -1,6 +1,7 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def best_fit_line(xs, ys):
 	m = ( (mean(xs) * mean(ys) - mean(xs * ys)) /
@@ -8,12 +9,42 @@ def best_fit_line(xs, ys):
 	b = mean(ys) - m * mean(xs)
 	return m, b
 
-xs = np.array([1,2,3,4,5])
-ys = np.array([5,4,6,5,6])
+def squared_error(ys_orig, ys_line):
+	return sum((ys_orig - ys_line)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+	y_mean_line = [mean(ys_orig) for _ in ys_orig]
+	squared_error_regression = squared_error(ys_orig, ys_line)
+	squared_error_mean = squared_error(ys_orig, y_mean_line)
+	return 1 - (squared_error_regression / squared_error_mean)
+
+def create_dataset(n, variance, step=2, correlation=None):
+	val = 1
+	ys = []
+	for i in range(n):
+		y = val + random.randrange(-variance, variance)
+		ys.append(y)
+		if correlation and correlation == 'pos':
+			val += step
+		elif correlation and correlation == 'neg':
+			val -= step
+
+	xs = [i for i in range(len(ys))]
+
+	return np.array(xs), np.array(ys)
+
+# xs = np.array([1,2,3,4,5])
+# ys = np.array([5,4,6,5,6])
+
+xs, ys = create_dataset(40, 40, correlation='pos')
 
 m, b = best_fit_line(xs, ys)
 
 regression_line = [m*x+b for x in xs]
+
+r2 = coefficient_of_determination(ys, regression_line)
+print(r2)
+
 plt.scatter(xs, ys)
 plt.plot(xs, regression_line)
 plt.show()
