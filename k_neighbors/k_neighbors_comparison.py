0834f8db32d1502ece1b3507bb73e0a1eb2073f9
Andrei Paleyes <apalees@gmail.com>
2016-11-12 02:13:00 +0000

    Implemented comparison of scikit and our own implementations of k nearest neighbors

---
+++ k_neighbors/k_neighbors_comparison.py
@@ -0,0 +1,75 @@
+import numpy as np
+import warnings
+from collections import Counter
+import pandas as pd
+import random
+from sklearn import preprocessing, model_selection, neighbors
+
+def k_neighbors(data, predict, k=3):
+	if len(data) >= k:
+		warnings.warn('K is less than the number of classes')
+
+	distances = []
+	for group in data:
+		for features in data[group]:
+			distances.append((np.linalg.norm(np.array(features) - np.array(predict)), group))
+	votes = [d[1] for d in sorted(distances)[:k]]
+	vote_result = Counter(votes).most_common(1)[0][0]
+	confidence = Counter(votes).most_common(1)[0][1] / k
+
+	return vote_result, confidence
+
+def prepare_data():
+	df = pd.read_csv("breast-cancer-wisconsin.data")
+	df.replace('?', -99999, inplace=True)
+	df.drop(['id'], 1, inplace=True)
+	return df
+
+def run_homebrew(df, n, test_size):
+	accuracies = []
+	for i in range(n):
+		full_data = df.astype(float).values.tolist()
+
+		random.shuffle(full_data)
+		
+		train_set = {2:[], 4:[]}
+		test_set = {2:[], 4:[]}
+		train_data = full_data[:-int(test_size*len(full_data))]
+		test_data = full_data[-int(test_size*len(full_data)):]
+
+		for i in train_data:
+			train_set[i[-1]].append(i[:-1])
+		for i in test_data:
+			test_set[i[-1]].append(i[:-1])
+
+		correct = 0
+		total = 0
+		for group in test_set:
+			for data in test_set[group]:
+				vote, confidence = k_neighbors(train_set, data, k=5)
+				if group == vote:
+					correct += 1
+				total += 1
+
+		accuracies.append(correct/total)
+
+	return np.mean(accuracies)
+
+def run_scikit(df, n, test_size):
+	accuracies = []
+	for i in range(n):
+		X = np.array(df.drop(['class'], 1))
+		y = np.array(df['class'])
+
+		X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=test_size)
+
+		clf = neighbors.KNeighborsClassifier()
+		clf.fit(X_train, y_train)
+		accuracy = clf.score(X_test, y_test)
+		accuracies.append(accuracy)
+
+	return np.mean(accuracies)
+
+df = prepare_data()
+print("Homebrew accuracy: {}".format(run_homebrew(df, 5, 0.2)))
+print("Scikit-learn accuracy: {}".format(run_scikit(df, 5, 0.2)))
