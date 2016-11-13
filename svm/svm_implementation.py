2e23b540d52da985cc7596feb72cf77673d428b5
Andrei Paleyes <apalees@gmail.com>
2016-11-13 23:51:53 +0000

    Partly implemented fitting of SVM

--- svm/svm_implementation.py
+++ svm/svm_implementation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
+import sys
 styles.use('ggplot')
 
 class SupportVectorMachine:
@@ -12,7 +13,34 @@ class SupportVectorMachine:
 			self.ax = self.figure.add_subplot(1, 1, 1)
 
 	def fit(self, data):
-		pass
+		self.data = data
+		opt_dict = {}
+
+		transforms = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+
+		self.max_feature_value = sys.maxsize
+		self.min_feature_value = -sys.maxsize
+		for yi in self.data:
+			for featureset in self.data[yi]:
+				self.max_feature_value = max(self.max_feature_value, max(featureset))
+				self.min_feature_value = min(self.min_feature_value, min(featureset))
+
+		step_sizes = [self.max_feature_value * 0.1,
+					  self.max_feature_value * 0.01,
+					  # high cost after this
+					  self.max_feature_value * 0.001]
+
+		# extremely expensive
+		b_range_multiple = 5
+		b_multiple = 5
+		latest_optimum = self.max_feature_value * 10
+
+		for step in step_sizes:
+			w = np.array([latest_optimum, latest_optimum])
+			optimized = False
+			while not optimized:
+				pass
+
 
 	def predict(self, features):
 		classification = np.sign(np.dot(np.array(features), self.w) + self.b)
