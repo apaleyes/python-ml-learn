0240258a05cd382f3e7f683adf13722e0a6207c0
Andrei Paleyes <apalees@gmail.com>
2016-11-22 16:40:35 +0000

    Actually commiting partial implementation

--- k_means/k_means_implementation.py
+++ k_means/k_means_implementation.py
@@ -10,5 +10,29 @@ X = np.array([[1, 2],
               [1, 0.6],
               [9,11]])
 
-plt.scatter(X[:,0], X[:,1], s=150)
-plt.show()
+class K_Means:
+	def __init__(self, k=2, tol=0.001, max_iter=300):
+		self.k = k
+		self.tolerance = tol
+		self.max_iter = max_iter
+
+	def fit(self, data):
+		self.centroids = {}
+		for i in range(self.k):
+			centroids[i] = data[i]
+
+		for i in range(self.max_iter):
+			self.classifications = {}
+
+			for i in range(self.k):
+				self.classifications[i] = []
+
+				for featureset in data:
+					distances = [np.linalg.norm(featureset - c) for c in self.centroids]
+					classification = distances.index(min(distances))
+					self.classifications[classification].append(featureset)
+
+				prev_centroids = dict(self.centroids)
+
+				for classification in self.classifications:
+					self.centroids[classification] = np.average(self.classifications[classification], axis=0)
