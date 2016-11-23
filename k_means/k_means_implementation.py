99c5b22bfaabd6deb20d433d40de622b7f3ccea4
Andrei Paleyes <apalees@gmail.com>
2016-11-23 01:27:16 +0000

    Finished k means implementation

--- k_means/k_means_implementation.py
+++ k_means/k_means_implementation.py
@@ -17,22 +17,68 @@ class K_Means:
 		self.max_iter = max_iter
 
 	def fit(self, data):
+		np.random.shuffle(data)
+
 		self.centroids = {}
 		for i in range(self.k):
-			centroids[i] = data[i]
+			self.centroids[i] = data[i]
 
 		for i in range(self.max_iter):
 			self.classifications = {}
 
-			for i in range(self.k):
-				self.classifications[i] = []
+			for j in range(self.k):
+				self.classifications[j] = []
 
-				for featureset in data:
-					distances = [np.linalg.norm(featureset - c) for c in self.centroids]
-					classification = distances.index(min(distances))
-					self.classifications[classification].append(featureset)
+			for featureset in data:
+				distances = [np.linalg.norm(featureset - self.centroids[c]) for c in self.centroids]
+				classification = distances.index(min(distances))
+				self.classifications[classification].append(featureset)
 
-				prev_centroids = dict(self.centroids)
+			prev_centroids = dict(self.centroids)
 
-				for classification in self.classifications:
-					self.centroids[classification] = np.average(self.classifications[classification], axis=0)
+			for classification in self.classifications:
+				self.centroids[classification] = np.average(self.classifications[classification], axis=0)
+
+			optimized = True
+			for c in self.centroids:
+				original_centroid = prev_centroids[c]
+				current_centroid = self.centroids[c]
+				if np.linalg.norm(current_centroid - original_centroid) / np.linalg.norm(original_centroid) * 100.0 > self.tolerance:
+					optimized = False
+					break
+
+			if optimized:
+				break
+
+	def predict(self, data):
+		distances = [np.linalg.norm(data - self.centroids[c]) for c in self.centroids]
+		classification = distances.index(min(distances))
+		return classification
+
+clf = K_Means(k=3)
+clf.fit(X)
+
+print(clf.centroids)
+
+for centroid in clf.centroids:
+	plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
+				marker="o", color="k", s=150, linewidths=5)
+
+colors = ["c", "b", "g", "r", "y", "m"]
+for cl in clf.classifications:
+	color = colors[cl]
+	for featureset in clf.classifications[cl]:
+		plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=100, linewidths=5)
+
+unknowns = np.array([[2,3],
+					 [10,10],
+					 [5,1],
+					 [1, 5],
+					 [3,8],
+					 [9, 6]])
+
+for unknown in unknowns:
+	cl = clf.predict(unknown)
+	plt.scatter(unknown[0], unknown[1], marker="*", color=colors[cl], s=100, linewidths=5)
+
+plt.show()
